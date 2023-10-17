ac48cebffa94d11c27338c0f9c326527c6179354
“Max <mparis@hotmail.cl>
2023-10-16 21:50:58 -0600

    some advacement in javadocs

--- src/main/requests/CreateGameRequest.java
+++ src/main/requests/CreateGameRequest.java
@@ -1,16 +1,33 @@
 package requests;
 
+/**
+ * A class to represent the JSON model passed to the CreateGameService
+ */
 public class CreateGameRequest {
+    /**
+     * Represents the name of the game to be created.
+     */
     private String gameName;
 
+    /**
+     * Constructs a new object.
+     * @param gameName initializes this.game to variable.
+     */
     public CreateGameRequest(String gameName) {
         this.gameName = gameName;
     }
 
+    /**
+     * @return the gameName.
+     */
     public String getGameName() {
         return gameName;
     }
 
+    /**
+     * Sets this.gameName to the parameter.
+     * @param gameName is the value that this.gameName will be assigned to.
+     */
     public void setGameName(String gameName) {
         this.gameName = gameName;
     }
--- src/main/requests/JoinGameRequest.java
+++ src/main/requests/JoinGameRequest.java
@@ -2,27 +2,56 @@ package requests;
 
 import chess.ChessPiece;
 
+/**
+ * A class to represent the JSON model passed to the JoinGameService
+ */
 public class JoinGameRequest {
+    /**
+     * the color of the player that will join the game
+     */
     private ChessPiece.PieceType playerColor;
+
+    /**
+     * the gameID of the game the player is trying to join.
+     */
     private int gameID;
 
+    /**
+     * Constructs a new object, with the parameters passed.
+     * @param playerColor is the value this.playerColor will be initialized to.
+     * @param gameID is the value this.gameID will be initialized to.
+     */
     public JoinGameRequest(ChessPiece.PieceType playerColor, int gameID) {
         this.playerColor = playerColor;
         this.gameID = gameID;
     }
 
+    /**
+     * @return the playerColor.
+     */
     public ChessPiece.PieceType getPlayerColor() {
         return playerColor;
     }
 
+    /**
+     * Sets the playerColor to the parameter value.
+     * @param playerColor is the value this.playerColor will be assigned to.
+     */
     public void setPlayerColor(ChessPiece.PieceType playerColor) {
         this.playerColor = playerColor;
     }
 
+    /**
+     * @return the gameID.
+     */
     public int getGameID() {
         return gameID;
     }
 
+    /**
+     * Sets the gameID to the parameter value.
+     * @param gameID is the value this.gameID will be assigned to.
+     */
     public void setGameID(int gameID) {
         this.gameID = gameID;
     }
